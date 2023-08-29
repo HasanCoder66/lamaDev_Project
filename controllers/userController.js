a94b348053b1c642fb831140b82c1ea5dfddba97
66CoderHasan <hasanashraf874@gmail.com>
2023-08-28 20:33:27 -0700

    login done

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,33 +11,37 @@ export const login = async (req, res) => {
         !user && res.status(404).json("User not found")
 
         const validPassword = await bcrypt.compare(req.body.password , user.password)
-        !password && res.status(404).json('Wrong password')
+        !validPassword && res.status(400).json('Wrong password')
+
+        res.status(200).json(user)
+
     } catch (error) {
-        console.log(error)
+        res.status(400).json(error) 
     }
 }
 
 // Register
 export const register = async (req, res) => {
-
+    
     try {
-
+        
         //Generate new password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(req.body.password, salt)
-
+        
         // Create New User
         const newUser = new User({
             userName: req.body.name,
             email: req.body.email,
             password: hashedPassword,
         })
+        
         // Save User and respond
         const user = await newUser.save()
         res.status(200).json(user)
     } catch (error) {
-
-        console.log(error)
+        
+        res.status(400).json(error) 
     }
 }
 
